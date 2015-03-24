d89a89b1f9e5e338a28c6565c9ed92f3186bdc7c
Santosh Thapa <t.syantos@gmail.com>
2015-03-23 22:58:20 -0400

    Privacy page and fixed bootstrap for edit and create

---
+++ GenProject/GlobalEventNepal/GlobalEventNepal.MVC/Controllers/HelpController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlobalEventNepal.MVC.Controllers
+{
+    public class HelpController : Controller
+    {
+        //
+        // GET: /Help/
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
